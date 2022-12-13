6a3473f29f3c3df22c9d7d3406951a0dd2f299fe
Adrian Sliacky <adko.sliacky@gmail.com>
2022-12-13 13:41:09 +0100

    AoC

--- 2022/05/main.py
+++ 2022/05/main.py
@@ -11,12 +11,12 @@ def sol(rever):
     crate_temp = deepcopy(crates)
     for line in part2:
         a, b = line.split(' from ')
-        s, t = [int(i) - 1 for i in b.split(' to ')]
-        r = [crate_temp[s].pop() for _i in range(int(a[5:]))]
+        c, d = [int(i) - 1 for i in b.split(' to ')]
+        r = [crate_temp[c].pop() for _ in range(int(a[5:]))]
         if rever:
-            crate_temp[t].extend(reversed(r))
+            crate_temp[d].extend(reversed(r))
         else:
-            crate_temp[t].extend(r)
+            crate_temp[d].extend(r)
     print(''.join(i.pop() for i in crate_temp))
 
 
