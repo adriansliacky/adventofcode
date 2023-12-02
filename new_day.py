d72a824ad713fd21f59d5279bd2503881992769a
Adrian Sliacky <adko.sliacky@gmail.com>
2023-12-02 17:23:55 +0100

    AoC

---
+++ new_day.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from aocd import get_data
+from aocd.models import Puzzle
+import os
+
+day, year = datetime.today().strftime('%d'), datetime.today().strftime('%Y')
+path = f'{year}/03'
+if not os.path.exists(path):
+    os.makedirs(path)
+
+example_data, p1_answer, p2_answer, *_ = Puzzle(year=int(year), day=int(day)).examples[0]
+
+if not os.path.exists(f'{path}/test.txt'):
+    with open(f'{path}/test.txt', 'w') as f:
+        f.writelines(example_data)
+        f.close()
+
+if not os.path.exists(f'{path}/input.txt'):
+    with open(f'{path}/input.txt', 'w') as f:
+        f.writelines(get_data())
+        f.close()
+
+if not os.path.exists(f'{path}/main.py'):
+    with open(f'{path}/main.py', 'w') as main:
+        with open('template.py') as template:
+            main.writelines(template.readlines())
+            template.close()
+        main.close()
---
+++ template.py
@@ -0,0 +1,30 @@
+from aocd import submit
+from aocd.models import Puzzle
+from datetime import datetime
+
+EXAMPLE = True
+PART = 1
+# ==================================================
+if EXAMPLE:
+    day, year = datetime.today().strftime('%d'), datetime.today().strftime('%Y')
+
+    example_data, p1_answer, p2_answer, *_ = Puzzle(year=int(year), day=int(day)).examples[0]
+    with open('test.txt') as file:
+        lines = file.read().strip().splitlines()
+
+else:
+    with open('input.txt') as file:
+        lines = file.read().strip().splitlines()
+# ==================================================
+
+total = 0
+for line in lines:
+    print(line)
+
+# ==================================================
+if EXAMPLE:
+    assert total == int(
+        p1_answer if PART == 1 else p2_answer), f'should be {p1_answer if PART == 1 else p2_answer}, got {total}'
+else:
+    print(total)
+    submit(total)
