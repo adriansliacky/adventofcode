c3453d3bf441e5aa58aa6f589004293bace79443
Adrian Sliacky <adko.sliacky@gmail.com>
2023-12-04 16:10:50 +0100

    simplification

--- 2023/02/main.py
+++ 2023/02/main.py
@@ -11,23 +11,17 @@ for line in lines:
     game_id = int(re.search(r"\d+", game_id).group(0))
     cubes = [x.strip() for x in re.split(', |;', cubes)]
 
+    color_map = {'red': (0, 12), 'green': (1, 13), 'blue': (2, 14)}
     maxes = [0, 0, 0]
     game_possible = True
+
     for c in cubes:
         n, color = c.split(' ')
         n = int(n)
-        if color == 'red':
-            maxes[0] = max(maxes[0], n)
-            if n > 12:
-                game_possible = False
-        elif color == 'green':
-            maxes[1] = max(maxes[1], n)
-            if n > 13:
-                game_possible = False
-        elif color == 'blue':
-            maxes[2] = max(maxes[2], n)
-            if n > 14:
-                game_possible = False
+        index, limit = color_map[color]
+        maxes[index] = max(maxes[index], n)
+        if n > limit:
+            game_possible = False
 
     if game_possible:
         part1 += game_id
