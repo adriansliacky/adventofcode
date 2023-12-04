be8f3d5ad24f3645493ba014681ac5423840585f
Adrian Sliacky <adko.sliacky@gmail.com>
2023-12-04 16:12:04 +0100

    simplification

--- 2023/02/main.py
+++ 2023/02/main.py
@@ -4,6 +4,7 @@ from math import prod
 with open('input.txt') as file:
     lines = file.read().strip().splitlines()
 
+COLOR_MAP = {'red': (0, 12), 'green': (1, 13), 'blue': (2, 14)}
 part1 = 0
 part2 = 0
 for line in lines:
@@ -11,14 +12,12 @@ for line in lines:
     game_id = int(re.search(r"\d+", game_id).group(0))
     cubes = [x.strip() for x in re.split(', |;', cubes)]
 
-    color_map = {'red': (0, 12), 'green': (1, 13), 'blue': (2, 14)}
-    maxes = [0, 0, 0]
+    maxes = [0] * 3
     game_possible = True
-
     for c in cubes:
         n, color = c.split(' ')
         n = int(n)
-        index, limit = color_map[color]
+        index, limit = COLOR_MAP[color]
         maxes[index] = max(maxes[index], n)
         if n > limit:
             game_possible = False
