62bb0d4415555089b05620833099dcd6d4c36712
Adrian <adko.sliacky@gmail.com>
2024-12-14 18:06:11 +0100

    Day 14

--- 2024/02/main.py
+++ 2024/02/main.py
@@ -1,9 +1,37 @@
-with open('test.txt') as file:
-    data = file.read().strip().splitlines()
-    data = [[int(y) for y in x.split()] for x in data]
+with open('input.txt') as file:
+    data = [[int(y) for y in x.split()] for x in file.read().strip().splitlines()]
 
-for row in data:
-    increasing = None
+
+def is_safe(row):
+    last = None
     increment = None
     for each in row:
-        if increasing is None and increment is None:
+        if last is None:
+            last = each
+            continue
+        elif increment is None:
+            inc = each - last
+            increment = inc > 0
+            if not 0 < abs(inc) < 4:
+                return False
+        else:
+            inc = each - last
+            if (inc > 0) != increment or not 0 < abs(inc) < 4:
+                return False
+        last = each
+    return True
+
+
+total = 0
+save_with_removals = 0
+for row in data:
+    if is_safe(row):
+        total += 1
+    else:
+        for i in range(len(row)):
+            if is_safe([row[y] for y in range(len(row)) if y != i]):
+                save_with_removals += 1
+                break
+
+print(total)
+print(total + save_with_removals)
